ede920553b11022c24eb55d8c3ad4674bd8c452a
Elazhari Salim <salim.elazhari@gmail.com>
2020-07-25 00:53:17 +0100

    teste

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl} from '@angular/forms';
+import {Chart} from 'chart.js';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+ADU:any;
+ASRLT:any;
+LT_Adjust:any;
+Var_Adjust:any;
+MOQ:any;
+Batch:any;  
+status:boolean=false;
+message:string="";
+zone_verte:any;
+zone_jaune:any;
+zone_rouge:any;
+zone_rouge_base:any;
+zone_rouge_securite:any;
+  constructor() { }
+
+  ngOnInit(): void {
+    
+
+  }
+  changed(){
+   
+   if(this.ADU && this.ASRLT && this.Batch && this.LT_Adjust && this.MOQ && this.Var_Adjust){
+     this.zone_verte=Math.max(this.MOQ, this.ASRLT*this.ADU*this.LT_Adjust, this.ADU*this.Batch);
+     this.zone_jaune=this.ADU*this.ASRLT;
+this.zone_rouge_base=this.ASRLT*this.ADU*this.LT_Adjust;
+this.zone_rouge_securite=this.zone_rouge_base*this.Var_Adjust;
+this.zone_rouge=this.zone_rouge_base+this.zone_rouge_securite;
+var myChart = new Chart('myChart', {
+  type: 'bar',
+  data: {
+      labels: ['Vert', 'Jaune', 'Rouge Base', 'Rouge sécurisé'],
+      datasets: [{
+          label: '',
+          data: [this.zone_verte, this.zone_jaune, this.zone_rouge_base, this.zone_rouge_securite],
+          backgroundColor: [
+              'rgba(0, 128, 0, 1)',
+              'rgba(255, 255 , 0, 1)',
+              'rgba(220, 20, 30, 1)',
+              'rgba(178,34,34, 1)',
+           
+          ],
+          borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+         
+          ],
+          borderWidth: 1
+      }]
+  },
+  options: {
+      scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero: true
+              }
+          }]
+      }
+  }
+});
+this.status=true;
+
+   }else{
+     this.message="Veuillez remplir tous les champs";
+      }
+  }
+
+}
